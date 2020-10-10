48658a01e51481b150226121022c107e274a31cc
Nagesh Sanika <nageshsanika@gmail.com>
2020-10-10 15:18:00 +0530

    test redux setup ( dispatch selector & logger)

--- src/containers/login/login.container.js
+++ src/containers/login/login.container.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 
-const LoginContainer = () => (
-  <div>
-    Login
-  </div>
-);
+import { authenticate } from '../../store/auth/actions';
+
+const LoginContainer = () => {
+  const { auth } = useSelector((store) => ({
+    auth: store.authStore,
+  }), shallowEqual);
+
+  console.log(auth);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(authenticate({}));
+  }, [dispatch]);
+
+  return (
+    <div>
+      Login
+    </div>
+  );
+}
 
 export default LoginContainer;
