332450e5a7acc4a4a215009dcb6c010ebd1194d1
Nagesh Sanika <nageshsanika@gmail.com>
2020-10-11 22:52:06 +0530

    Updates

---
+++ precache-manifest.6b8f3836e74757b4d6e743f4e40ce47a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d8cdf7eb72f670c083df84429935f94",
+    "url": "/automate.flow/index.html"
+  },
+  {
+    "revision": "66ecc49e6882c0bd68b4",
+    "url": "/automate.flow/static/css/main.7333c7b5.chunk.css"
+  },
+  {
+    "revision": "d31055f7afc73cbd18cc",
+    "url": "/automate.flow/static/js/2.0d9f6dae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/automate.flow/static/js/2.0d9f6dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ecc49e6882c0bd68b4",
+    "url": "/automate.flow/static/js/main.0c30f5cd.chunk.js"
+  },
+  {
+    "revision": "fd9a06632edb0a852fc8",
+    "url": "/automate.flow/static/js/runtime-main.ce22fd26.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f79d646b8b8c3b76d7ef4200fd2852c6.js"
+  "/automate.flow/precache-manifest.6b8f3836e74757b4d6e743f4e40ce47a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/automate.flow/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
