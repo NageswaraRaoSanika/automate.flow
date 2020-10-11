5fbccee6a801ff4fbda4e635f1389bb45205d4be
Nagesh Sanika <nageshsanika@gmail.com>
2020-10-11 22:17:42 +0530

    Status Icon Improvemennts

--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -7,21 +7,35 @@ type Props = {
   name: String, 
   description: String,
   status: String,
+  updateStaus: void => null,
 }
 
-const WorkflowCard = (props: Props) => {
-
+const CARD_STATUS_COLORS = {
+  Pending: {
+    background: '#f2f2f2',
+    color: '#000'
+  },
+  InProgress: {
+    background: 'rgb(26, 115, 232)',
+    color: '#fff',
+  },
+  Completed: {
+    background: 'green',
+    color: '#fff',
+  }
+}
+const Card = (props: Props) => {
   return (
     <div className={styles.workflowCard}>
       <div
         className={styles.statusIcon}
         onClick={(e) => {
           e.preventDefault();
-          alert(props.status);
+          props.updateStaus(props.status);
         }}
-        style={{background: props.status === 'Pending' ? '#f2f2f2' : 'green'}}
+        style={{...CARD_STATUS_COLORS[props.status]}}
       >
-        <StatusIcon color={props.status === 'Pending' ? 'black' : 'white'} />
+        <StatusIcon />
       </div>
       <div className={styles.title}>
         {props.title}
@@ -36,4 +50,4 @@ const WorkflowCard = (props: Props) => {
   );
 };
 
-export default WorkflowCard;
+export default Card;
