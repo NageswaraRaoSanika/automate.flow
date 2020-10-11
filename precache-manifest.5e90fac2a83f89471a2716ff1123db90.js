eeb5b2e56833f4fa35acb44e1a86dd91be91150c
Nagesh Sanika <nageshsanika@gmail.com>
2020-10-11 22:40:47 +0530

    Updates

---
+++ precache-manifest.5e90fac2a83f89471a2716ff1123db90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df3bbbbad8c380b6b9aa62acb9215f90",
+    "url": "/automate.flow/index.html"
+  },
+  {
+    "revision": "044bf592b1238efed2b0",
+    "url": "/automate.flow/static/css/main.7333c7b5.chunk.css"
+  },
+  {
+    "revision": "d31055f7afc73cbd18cc",
+    "url": "/automate.flow/static/js/2.0d9f6dae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/automate.flow/static/js/2.0d9f6dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "044bf592b1238efed2b0",
+    "url": "/automate.flow/static/js/main.6a49e9ea.chunk.js"
+  },
+  {
+    "revision": "fd9a06632edb0a852fc8",
+    "url": "/automate.flow/static/js/runtime-main.ce22fd26.js"
+  }
+]);
